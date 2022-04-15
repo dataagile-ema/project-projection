afc8823d498bfe271aa304a578351ed369179017
data_agile <niklas3000@gmail.com>
2022-04-15 13:34:36 +0200

    snyggat till texter och layout, lagt till rework

--- app.py
+++ app.py
@@ -21,19 +21,19 @@ def str2num(s):
 
 # sida
 im = Image.open("favicon.ico")
-st.set_page_config(page_title="Project projection", page_icon=im)
+st.set_page_config(page_title="Projektprognos", page_icon=im)
 """
-# Project projection
+# Projektprognos
 """
 
 left, right = st.columns(2)
 with left:
-
-    min_for_story = str2num(st.text_input("Min för en User Story", value="5", key="text"))
-    no_of_stories = str2num(st.text_input("Antal User Stories", value="10", key="text"))    
+    min_for_story = str2num(st.text_input("Min tid för en User Story", value="5"))
+    max_for_story = str2num(st.text_input("Max tid för en User Story", value="40"))
+   
 with right:
-    max_for_story = str2num(st.text_input("Max för en User Story", value="15", key="text"))
-    share_rework = str2num(st.text_input("Procent av User Stories som är Rework", value="10", key="text"))
+    no_of_stories = str2num(st.text_input("Antal User Stories", value="10", key="text")) 
+    share_rework = str2num(st.text_input("Procent av all tid som är rättningar", value="10"))
 
 
 #st.write(no_of_stories, min_for_story, max_for_story)
@@ -49,12 +49,22 @@ if no_of_stories > 0.0 and min_for_story > 0.0 and max_for_story > 0.0:
 
     df["value"] = [calc_uniform_simulation(int(no_of_stories), min_for_story, max_for_story) for i in range(5000)]
 
-        
+    factor_rework = 1 + (share_rework/100)
+
+    # multiply by factor
+    df["value"] = df["value"] * factor_rework
 
 
     plt.style.use('ggplot')
+    plt.rcParams.update({'font.size': 14})
 
     fig, ax = plt.subplots()
+
+    # remove scale on y-axis
+    #ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+    # remove y-axis
+    ax.yaxis.set_visible(False)
 
     # dist plot without label for 'value'
     ax.hist(df["value"], bins=20, density=True, alpha=0.5)
@@ -67,9 +77,15 @@ if no_of_stories > 0.0 and min_for_story > 0.0 and max_for_story > 0.0:
     p85th = df.value.quantile(0.85)
     p50th = df.value.quantile(0.5)
 
-    ax.axvline(p85th, color='g', linestyle='--', label=f'85th percentile: {round(p85th,0)}')
+    # set label_str to p85th rounded to 0 decimals
+    label_str_85 = "{:.0f}".format(p85th)
+    label_str_50 = "{:.0f}".format(p50th)
 
-    ax.axvline(p50th, color='b', linestyle='--', label=f'50th percentile: {round(p50th,0)}')
+
+
+    ax.axvline(p85th, color='g', linestyle='--', label=f'85:e procentilen: {label_str_85}')
+
+    ax.axvline(p50th, color='b', linestyle='--', label=f'50:e percentile: {label_str_50}')
 
 
 
@@ -77,7 +93,8 @@ if no_of_stories > 0.0 and min_for_story > 0.0 and max_for_story > 0.0:
 
     # set height of plot
     fig.set_size_inches(10, 5)
-
+    # set font size
+  
     st.pyplot(fig)
 
 
